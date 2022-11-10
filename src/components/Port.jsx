eecb9d6dc8e6bcd2cb90ef85ca780bd9e43cf64e
efss7 <ericfelipess07@gmail.com>
2022-11-10 15:36:58 -0300

    Componente Porta #03

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -2,9 +2,11 @@ import styles from '../styles/Port.module.css'
 export default function Port(props) {
 
     return (
-        <div className={styles.port}>
-            <div className={styles.number}>
-                3
+        <div className={styles.area}>
+            <div className={styles.frame}>
+                <div className={styles.port}>
+                    <div className={styles.number}>3</div>
+                </div>
             </div>
         </div>
     )
