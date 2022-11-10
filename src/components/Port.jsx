5438ebb7e237fc03bc4695fc7aa4ab464dd0106b
efss7 <ericfelipess07@gmail.com>
2022-11-10 16:05:49 -0300

    Componente Porta #06

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,9 +1,9 @@
 import styles from '../styles/Port.module.css'
 export default function Port(props) {
-
+    const selected = props.selected ? styles.selected: ''
     return (
         <div className={styles.area}>
-            <div className={styles.frame}>
+            <div className={`${styles.frame} ${selected}`}>
                 <div className={styles.port}>
                     <div className={styles.number}>3</div>
                     <div className={styles.doorHandle}></div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,8 @@ import Port from "../components/Port";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: "flex" }}>
+      <Port selected />
       <Port />
     </div>
   )
