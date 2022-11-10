dab7137b461307f7c192331f0848eed0567952de
efss7 <ericfelipess07@gmail.com>
2022-11-10 14:56:20 -0300

    Componente Presente

---
+++ src/components/Gift.jsx
@@ -0,0 +1,23 @@
+import styles from '../styles/Gift.module.css';
+
+export default function Gift() {
+    return (
+        <div className={styles.gift}>
+            <div className={styles.cover}>
+
+            </div>
+
+            <div className={styles.body}>
+
+            </div>
+
+            <div className={styles.link1}>
+
+            </div>
+
+            <div className={styles.link2}>
+
+            </div>
+        </div>
+    )
+}
