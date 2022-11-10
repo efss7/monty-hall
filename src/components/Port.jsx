1fbb219e792a6ad56c6569f5c40a08ccb4524bcb
efss7 <ericfelipess07@gmail.com>
2022-11-10 15:56:06 -0300

    Componente Porta #05

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -6,6 +6,8 @@ export default function Port(props) {
             <div className={styles.frame}>
                 <div className={styles.port}>
                     <div className={styles.number}>3</div>
+                    <div className={styles.doorHandle}></div>
+
                 </div>
             </div>
             <div className={styles.floor}></div>
