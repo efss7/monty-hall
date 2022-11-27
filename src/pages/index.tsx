966e0a55fe9f4a7c6e71cc9d811f10f181d8e3c7
efss7 <ericfelipess07@gmail.com>
2022-11-26 23:20:35 -0300

    Estruturando o Formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
-import Port from "../components/Port";
-import { createPorts, updatePort } from "../functions/PortFunctions";
+import styles from "../styles/Form.module.css";
+import Card from "../components/Card";
+import Link from "next/link";
 
-export default function Home() {
-  return(
-    <h1>Formulário de início</h1>
-  )
+export default function Form() {
+    return (
+        <div className={styles.form}>
+            <div>
+                <Card bgcolor="#c0392c">
+                    <h1>Monty Hall</h1>
+                </Card>
+                <Card> </Card>
+            </div>
+            <div>
+                <Card> </Card>
+                
+                <Card bgcolor="#28a085" >
+                    <Link href={`/game/4/2`} >
+                        <h2 className={styles.link}>Iniciar Jogo</h2>
+                    </Link>
+                </Card>
+            </div>
+        </div>
+    )
 }
